430695d14416911e89e40d59af4a5d833ab797ab
Saisri1994 <37657161+Saisri1994@users.noreply.github.com>
2021-11-13 18:44:33 +0000

    Add files via upload

---
+++ alb-target-groups/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = ""
+}
+variable "alb-tg-name" {
+  default = ""
+}
+variable "target-group-port" {
+  default = ""
+}
+variable "target-group-protocol" {
+  default = ""
+}
+variable "vpc-id" {
+  default = ""
+}
